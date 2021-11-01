11ecc3b45d6f34e46c7caf7973b0ffd8ad4656d4
ZiruiNiu <92980194+ZiruiNiu@users.noreply.github.com>
2021-11-01 15:43:39 +0800

    Delete pathObj.m

--- OptimalControl_FourScenarios/pathObj.m
+++
@@ -1,5 +0,0 @@
-function J = pathObj(x, u, x_tar, Q, R)
-% Define the Lagrangian term in the cost function
-% It's a quadratic form in our design
-    J = sum(Q*((x - x_tar).^2)) + sum(R*(u.^2));
-end
